e53c52b6879cbc25a18a42f0ffeca2ede1577112
psych0wh1z <ikeagudike@gmail.com>
2024-03-03 23:20:59 +0100

    import useEffect hook

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/Editor.css'
 
 export default function Editor() {
     const [text, setText] = useState('')
 
+    useEffect(
+
+    )
+
     return (
         <>
-            <textarea name="" id="editor" cols="30" rows="10"></textarea>
+            <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
         </>
     )
 }
