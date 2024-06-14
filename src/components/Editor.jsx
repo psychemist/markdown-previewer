095da74e75e2c6ba74aa1864358b16e441f56b0a
psych0wh1z <ikeagudike@gmail.com>
2024-06-14 17:06:44 +0100

    Move default markdown to App component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,56 +3,9 @@ import '../styles/Editor.css'
 export default function Editor({ text, changeText }) {
   return (
     <textarea
-      value={text}
-      name="editor"
       id="editor"
-      onChange={changeText(markup)}>
+      value={text}
+      onChange={changeText}>
     </textarea>
   )
 }
-
-// const defaultMarkdown = ```
-// # Welcome to my React Markdown Previewer!
-
-// ## This is a sub-heading...
-// ### And here's some other cool stuff:
-
-// Heres some code, ``, between 2 backticks.
-
-// ```
-// // this is multi-line code:
-
-// function anotherExample(firstLine, lastLine) {
-//     if (firstLine == '```' && lastLine == '```') {
-//         return multiLineCode;
-//     }
-// }
-// ```
-
-// You can also make text **bold**... whoa!
-// Or _italic_.
-// Or... wait for it... **_both!_**
-// And feel free to go crazy ~~crossing stuff out~~.
-
-// There's also [links](https://www.freecodecamp.org), and
-// > Block Quotes!
-
-// And if you want to get really crazy, even tables:
-
-// Wild Header | Crazy Header | Another Header?
-// ------------ | ------------- | -------------
-// Your content can | be here, and it | can be here....
-// And here. | Okay. | I think we get it.
-
-// - And of course there are lists.
-//   - Some are bulleted.
-//      - With different indentation levels.
-//         - That look like this.
-
-
-// 1. And there are numbered lists too.
-// 1. Use just 1s if you want!
-// 1. And last but not least, let's not forget embedded images:
-
-// ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-// ```
